cb9e28e0f105f92aecec2ef434e0a5d6e120360f
Abuitime <alex.buitimea13@gmail.com>
2015-02-20 14:16:11 -0800

    Completed Lab7

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,11 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
+  models.Project.find({_id: projectID}, function(err, projects) {
+    if (err) console.log(err);
+    res.json(projects[0]);
 
+  });
   // query for the specific project and
   // call the following callback
 
@@ -16,13 +20,28 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  (new models.Project({
+    title: form_data.project_title,
+    date:  form_data.date,
+    summary: form_data.summary,
+    image: form_data.image_url
+  })).save(function(err) {
+    if (err) console.log(err);
+    res.send();
+  });
+
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
-
+  models.Project.find({_id: projectID})
+                .remove()
+                .exec(function(err) {
+                  if (err) console.log(err);
+                  res.send();
+                });
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
